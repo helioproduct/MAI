106871f01d29a8340b62a1111289367f5a75aaaa
Nikolay Popov <helioproduct.sync@gmail.com>
2022-01-17 12:59:55 +0300

    Emam tasks: 2th added

---
+++ 1sem/exam/tasks/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Проверка палиндрома числа
+//  Сложность: O(log n)
+
+bool is_palindrom(long long number)
+{  
+    long long saved_number, reversed_number;
+    saved_number = number;
+    reversed_number = 0;
+
+    while (number > 0)
+    {
+        reversed_number = reversed_number * 10 + number % 10;
+        number /= 10;
+    }
+
+    return saved_number == reversed_number;
+}
+
+int main()
+{
+    long long number;
+    scanf("%lld", &number);
+    printf(is_palindrom ? "YES" : "NO");
+    return 0;
+}
