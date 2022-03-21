088a9bc4935a1cc7d2fbf518f9f5096e29c3d18a
helioproduct <helioproduct.sync@gmail.com>
2022-03-21 17:59:11 +0500

    bin-tree structures added

---
+++ 2sem/lab23/tree.h
@@ -0,0 +1,15 @@
+#ifndef _TREE_H_
+#define _TREE_H_
+
+#include <node.h>
+
+typedef struct Tree {
+	Node root;
+} Tree;
+
+Tree *create_tree(Node root_value);
+Tree *tree_add_node(Tree *tree);
+void print_tree(Tree *tree);
+
+
+#endif // _TREE_H_
