98eca1d91f0f0294f7cd670f82e3d2672525e69f
helio <helioproduct.sync@gmail.com>
2022-04-03 17:05:25 +0300

    lab23 file guard added

---
+++ 2sem/lab23/tree.c
@@ -0,0 +1,17 @@
+#include "tree.h"
+#include <stdio.h>
+
+Tree *create_tree(Node root_value)
+{
+
+}
+
+Tree *add_node(Tree *tree)
+{
+
+}
+
+void print_tree(Tree *tree) 
+{
+	printf("%s", "hey");
+}
