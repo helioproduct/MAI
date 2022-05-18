450ea219f40716f649352f6bcb0c89a90c4db801
helio <helioproduct.sync@gmail.com>
2022-05-18 20:53:13 +0300

    DM: project structure added

---
+++ 2sem/DM/main.py
@@ -0,0 +1,20 @@
+import numpy
+
+def destroy_circuits(g):
+    pass
+
+
+def complement_to_transitive_closure(g):
+    pass
+
+
+def complement_to_linear_order(g):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
